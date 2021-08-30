7a21039aa5f56cb95a0a5171322684ce10869ae3
RadhwaneDZ <71345996+RadhwaneDZ@users.noreply.github.com>
2021-08-30 16:50:30 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,9 +2,11 @@ fx_version 'cerulean'
 games { 'gta5' }
 author 'MOXHA'
 server_only 'yes'
-
+shared_scripts { 
+	'@qb-core/import.lua',
+}
 server_scripts {
-     '@mysql-async/lib/MySQL.lua',
+     
      'server.lua',
      'bot.js'
 } 
